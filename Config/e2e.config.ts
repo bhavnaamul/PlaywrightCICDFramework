ff6833b8b708900a29c64fdf4c51df57c2e8ac70
Bhavna <bhavna.amul@gmail.com>
2026-08-31 22:19:52 +0530

    test github webhook

--- Config/e2e.config.ts
+++ Config/e2e.config.ts
@@ -2,27 +2,17 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   timeout: 60000,
-  retries: 0,
+  retries: process.env.CI ? 2 : 0,
   testDir: '../tests/E2E',
   fullyParallel: true,
+  forbidOnly: !!process.env.CI,
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['allure-playwright'],
-    // [
-    //   'junit',
-    //   {
-    //     outputFile: 'test-results/results.xml',
-    //   },
-    // ],
-    // [
-    //   'json',
-    //   {
-    //     outputFile: 'test-results/results.json',
-    //   },
-    // ],
   ],
-  workers: 10,
+  workers: process.env.CI ? 4 : 10,
+
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
@@ -30,7 +20,7 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
     video: 'off',
     screenshot: 'on',
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
